e88b9b5b9f3abdf3445229167deff7eb2a49bdfb
jcor25 <153782427+jcor25@users.noreply.github.com>
2025-02-20 01:01:16 -0600

    Update hellen_leds_144.cpp

--- firmware/config/boards/hellen/hellen_leds_144.cpp
+++ firmware/config/boards/hellen/hellen_leds_144.cpp
@@ -2,11 +2,11 @@
 #include "hellen_meta.h"
 
 Gpio getCommsLedPin() {
-	return H144_LED3_BLUE;
+	return H144_LED2_GREEN ;
 }
 
 Gpio getRunningLedPin() {
-	return H144_LED2_GREEN;
+	return H144_LED3_BLUE;
 }
 
 Gpio getWarningLedPin() {
